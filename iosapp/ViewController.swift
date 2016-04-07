b6ab6ee5a5739032f7857d880db75d134afd7449
Iris <iris@iriss-macbook-pro.local>
2016-04-07 19:26:43 +0800

    Initial Commit

---
+++ iosapp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  iosapp
+//
+//  Created by Iris on 2016/4/7.
+//  Copyright © 2016年 leo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
